819524b8b29553ea3e77b55d7fb233ed7eefda1c
msahadl60 <msahadl60@gmail.com>
2015-01-10 19:49:30 +0100

    adapted remotedebug for useage of addon script.module.pydevd in Linux

--- default.py
+++ default.py
@@ -9,18 +9,19 @@ import xbmc
 __remotedebug__ = False
 # Append pydev remote debugger
 if __remotedebug__:
+    utils.log("Initialize remote debugging.")
     # Make pydev debugger works for auto reload.
-    # Note pydevd module need to be copied in XBMC\system\python\Lib\pysrc
     try:
-        import pysrc.pydevd as pydevd
-    # stdoutToServer and stderrToServer redirect stdout and stderr to eclipse console
+        import pydevd
         pydevd.settrace('localhost', port=60678, stdoutToServer=True, stderrToServer=True)
     except ImportError:
         sys.stderr.write("Error: " +
             "You must add org.python.pydev.debug.pysrc to your PYTHONPATH.")
+        utils.showNotification('WatchedList Error', 'remote debug could not be imported.')
         sys.exit(1)
     except:
-        sys.stderr.write("Error importing the debugger")
+        utils.showNotification('WatchedList Error', 'remote debug in pydev is activated, but remote server not responding.')
+        sys.exit(1)
 
 # Run the program
 xbmc.sleep(1500) # wait 1.5 seconds to prevent import-errors
